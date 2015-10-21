8dc8678337058197354b35fe211b8f291cf492e2
Ian Tuomi <ian.tuomi@gofore.com>
2015-10-21 11:49:53 +0300

    LK-8: Add browser-sync to automatically reload styles

--- ckanext/ckanext-apicatalog_ui/watchless.sh
+++ ckanext/ckanext-apicatalog_ui/watchless.sh
@@ -1,1 +1,3 @@
-fswatch -o less | xargs -n1 -I{} ./buildless.sh
+fswatch -o less | xargs -n1 -I{} ./buildless.sh &
+browser-sync start --proxy "10.100.10.10" --files "ckanext/apicatalog_ui/public/main.css" &
+
