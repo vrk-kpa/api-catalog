67329f9713cfb0bd14b7f9517d540f0e8f22074b
Teemu Erkkola <teemu.erkkola@gofore.com>
2015-12-18 14:38:23 +0200

    LK-33: Uppercase file extensions

--- ckanext/ckanext-apicatalog_scheming/ckanext/apicatalog_scheming/plugin.py
+++ ckanext/ckanext-apicatalog_scheming/ckanext/apicatalog_scheming/plugin.py
@@ -1,9 +1,11 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+import validators
 
 
 class Apicatalog_SchemingPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IValidators)
 
     # IConfigurer
 
@@ -11,3 +13,9 @@ class Apicatalog_SchemingPlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'apicatalog_scheming')
+
+    def get_validators(self):
+        return {
+            'lower_if_exists': validators.lower_if_exists,
+            'upper_if_exists': validators.upper_if_exists
+            }
---
+++ ckanext/ckanext-apicatalog_scheming/ckanext/apicatalog_scheming/validators.py
@@ -0,0 +1,6 @@
+def lower_if_exists(s):
+    return s.lower() if s else s
+
+
+def upper_if_exists(s):
+    return s.upper() if s else s
