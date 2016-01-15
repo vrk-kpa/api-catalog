c66f8a0849358d9974d8717fcd31edb49b213cf9
Teemu Erkkola <teemu.erkkola@gofore.com>
2016-01-15 15:11:14 +0200

    LK-100: Restrict revision diffs and package revision lists

--- ckanext/ckanext-apicatalog_routes/ckanext/apicatalog_routes/plugin.py
+++ ckanext/ckanext-apicatalog_routes/ckanext/apicatalog_routes/plugin.py
@@ -20,23 +20,37 @@ class Apicatalog_RoutesPlugin(ckan.plugins.SingletonPlugin):
     def before_map(self, m):
         controller = 'ckanext.apicatalog_routes.plugin:Apicatalog_RevisionController'
         m.connect('/revision/list', action='list', controller=controller)
+        m.connect('/revision/diff/{id}', action='diff', controller=controller)
         return m
 
     # IAuthFunctions
 
     def get_auth_functions(self):
         return {'user_list': admin_only,
-                'revision_list': admin_only
+                'revision_list': admin_only,
+                'revision_diff': admin_only,
+                'package_revision_list': admin_only
                 }
 
 
+def auth_context():
+    return {'model': ckan.model,
+            'user': c.user or c.author,
+            'auth_user_obj': c.userobj}
+
+
 class Apicatalog_RevisionController(RevisionController):
+
     def list(self):
-        context = {'model': ckan.model,
-                   'user': c.user or c.author,
-                   'auth_user_obj': c.userobj}
         try:
-            ckan.logic.check_access('revision_list', context)
+            ckan.logic.check_access('revision_list', auth_context())
             return super(Apicatalog_RevisionController, self).list()
+        except ckan.logic.NotAuthorized:
+            ckan.lib.base.abort(401, _('Not authorized to see this page'))
+
+    def diff(self, id=None):
+        try:
+            ckan.logic.check_access('revision_diff', auth_context())
+            return super(Apicatalog_RevisionController, self).diff(id=id)
         except ckan.logic.NotAuthorized:
             ckan.lib.base.abort(401, _('Not authorized to see this page'))
