457d5e70f4aaa4396bc5689aa1e9ee0b8ed6362c
Teemu Erkkola <teemu.erkkola@gofore.com>
2016-02-12 15:04:16 +0200

    LK-114: Added CSRF tokens and checking to all needed forms

--- tools/check_csrf.sh
+++ tools/check_csrf.sh
@@ -2,12 +2,15 @@
 
 # A script for checking if there are forms that need csrf handling
 
-HELPTEXT="Usage: $0 [whitelist file]\n\n\
+HELPTEXT="Usage: $0 [--help] [--done] [whitelist file]\n\n\
 Finds HTML files with forms without mentions of CSRF tokens.\n\
-Run at the root of the directory tree to scan. To exclude already cleared files from the results provide a whitelist file.\n"
+Run at the root of the directory tree to scan. To exclude already cleared files from the results provide a whitelist file. List forms with CSRF tokens with 'done'.\n"
 
 if [ $1 = "--help" ]; then
   echo -e $HELPTEXT
+elif [ $1 = "--done" ]; then
+  grep --include "*.html" -Rli "<form" . \
+    | xargs grep -li "csrf_token"
 else
   grep --include "*.html" -Rli "<form" . \
     | xargs grep -Li "csrf_token" \
