91e34b1be9ba6cb4d9c687a798ecc4c6f8db9bc9
Teemu Erkkola <teemu.erkkola@gofore.com>
2016-04-25 08:22:27 +0300

    Fix handling APIs with no shared resource information

--- ckanext/ckanext-apicatalog_ui/ckanext/apicatalog_ui/plugin.py
+++ ckanext/ckanext-apicatalog_ui/ckanext/apicatalog_ui/plugin.py
@@ -41,7 +41,7 @@ def get_homepage_organizations(count=1):
         if group['package_count'] is 0:
             continue
         # skip orgs with 1 package, if shared resource is "no"
-        if group['package_count'] is 1 and group['packages'][0]['shared_resource'] == "no":
+        if group['package_count'] is 1 and group['packages'][0].get('shared_resource', 'no') == 'no':
             continue
         found.append(group['id'])
         groups_data.append(group)
