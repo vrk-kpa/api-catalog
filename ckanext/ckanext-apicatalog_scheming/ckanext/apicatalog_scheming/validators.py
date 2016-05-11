92c3341ea1f3b9bdd090310229b4cf61a83dcd82
Jari Voutilainen <jari.voutilainen@iki.fi>
2016-05-11 12:29:30 +0300

    LK-184: refactor number_of_saves to apply only package or dataset

--- ckanext/ckanext-apicatalog_scheming/ckanext/apicatalog_scheming/validators.py
+++ ckanext/ckanext-apicatalog_scheming/ckanext/apicatalog_scheming/validators.py
@@ -5,5 +5,11 @@ def lower_if_exists(s):
 def upper_if_exists(s):
     return s.upper() if s else s
 
-def increment_if_exists(number):
-    return int(number) + 1 if number else 0
+def increment_if_exists(data, context):
+    if 'save' in context:
+        return int(data) + 1 if data else 0
+    return data
+
+def resource_validator(data):
+    print data
+    return int(data) + 1 if data else 0
