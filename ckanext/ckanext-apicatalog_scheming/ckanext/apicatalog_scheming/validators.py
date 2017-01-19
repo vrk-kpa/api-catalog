c4ee41c8902d8e2b5e554005edc16532ff3add48
Teemu Erkkola <teemu.erkkola@gofore.com>
2017-01-19 10:38:07 +0200

    Hotfix to allow both dict and object packages in validator context for valid_resources

--- ckanext/ckanext-apicatalog_scheming/ckanext/apicatalog_scheming/validators.py
+++ ckanext/ckanext-apicatalog_scheming/ckanext/apicatalog_scheming/validators.py
@@ -10,16 +10,20 @@ def upper_if_exists(s):
     return s.upper() if s else s
 
 
+def get(field, obj):
+    return obj[field] if type(obj) is dict else obj.__getattribute__(field)
+
+
 def valid_resources(private, context):
     package = context.get('package')
     if not package:
         return private
 
-    change = package.get('private') != private
+    change = get('private', package) != private
     to_public = private is False or private == u'False'
 
     if change and to_public:
-        for resource in package.resources:
-            if resource.extras.get('valid_content') == 'no':
+        for resource in get('resources', package):
+            if get('extras', resource).get('valid_content') == 'no':
                 raise df.Invalid(_("Package contains invalid resources"))
     return private
