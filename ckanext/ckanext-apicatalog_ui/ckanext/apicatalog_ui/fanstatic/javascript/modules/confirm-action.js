4a1bf42f6827131227903afca377fade32f39c12
Teemu Erkkola <teemu.erkkola@gofore.com>
2019-04-12 09:47:55 +0300

    Fix CSRF checking for organization member list

--- ckanext/ckanext-apicatalog_ui/ckanext/apicatalog_ui/fanstatic/javascript/modules/confirm-action.js
+++ ckanext/ckanext-apicatalog_ui/ckanext/apicatalog_ui/fanstatic/javascript/modules/confirm-action.js
@@ -83,6 +83,12 @@ this.ckan.module('confirm-action', function (jQuery) {
         action: this.el.attr('href'),
         method: 'POST'
       });
+      var csrf = jQuery('<input/>', {
+        type: 'hidden',
+        name: 'csrf-token',
+        value: $('form[method=POST]').find('input[type=hidden][name=csrf-token]').attr('value') || this.el.attr('csrf-token')
+      });
+      csrf.appendTo(form);
       form.appendTo('body').submit();
     },
 
