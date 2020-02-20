1232462ce1e8cd4a5acba35a86ec899bd8de8c76
Jari Voutilainen <jari.voutilainen@iki.fi>
2020-02-20 13:21:11 +0200

    Add replacer script for fallback free font

--- ckanext/ckanext-apicatalog_ui/gulpfile.js
+++ ckanext/ckanext-apicatalog_ui/gulpfile.js
@@ -1,12 +1,18 @@
 const { src, dest, parallel } = require('gulp');
+const fs = require('fs');
+
+let fontawesomePath = './node_modules/@fortawesome/fontawesome-pro';
+if (!fs.existsSync('./node_modules/@fortawesome/fontawesome-pro')){
+    fontawesomePath = './node_modules/@fortawesome/fontawesome-free'
+}
 
 function fontAwesomeCss() {
-    return src('./node_modules/@fortawesome/fontawesome-pro/css/*.css')
+    return src(fontawesomePath + '/css/*.css')
         .pipe(dest("ckanext/apicatalog_ui/fanstatic/font-awesome/css/"))
 }
 
 function fontAwesomeFonts() {
-    return src('./node_modules/@fortawesome/fontawesome-pro/webfonts/*')
+    return src(fontawesomePath + '/webfonts/*')
         .pipe(dest("ckanext/apicatalog_ui/fanstatic/font-awesome/webfonts/"))
 }
 
