b0af0bd297d45b92803630336f3205bfed3ed294
Jari Voutilainen <jari.voutilainen@iki.fi>
2020-05-11 08:39:06 +0300

    LIKA-106: Remove placeholder code

--- ckanext/ckanext-apply_permissions_for_service/ckanext/apply_permissions_for_service/logic/action/get.py
+++ ckanext/ckanext-apply_permissions_for_service/ckanext/apply_permissions_for_service/logic/action/get.py
@@ -25,24 +25,3 @@ def service_permission_application_show(context, data_dict):
     application = model.ApplyPermission.get(application_id).as_dict()
     return application
 
-    packages = plugins.toolkit.get_action('package_search')(context, {}).get('results', [])
-    fake_count = 10
-    random_from = packages[int(application_id) % len(packages)]
-    random_to = packages[(7 * int(application_id) + 3) % len(packages)]
-    return {
-        'id': str(application_id),
-        'from': {
-            'subsystem': random_from,
-            },
-        'to': {
-            'subsystem': random_to,
-            'services': random_to['resources']
-            },
-        'organization': random_from.get('organization', {}).get('name'),
-        'business_code': '%07d-%d' % (int(application_id) % 10000000, int(application_id) % 10),
-        'contact_name': 'Person %s' % application_id,
-        'contact_email': 'person%s@%s.doesnot.exist' % (application_id, random_from['organization']['name']),
-        'ip_addresses': ['127.0.0.1', '10.0.0.1'],
-        'usage_description': 'Lorem ipsum, dolor sit amet.',
-        'request_date': '2020-01-01'
-        }
