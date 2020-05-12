6bb2267d601164de89bea2056af0289e7494faae
Teemu Erkkola <teemu.erkkola@gofore.com>
2020-05-12 13:34:55 +0300

    LIKA-104: Added 403 error management

--- ckanext/ckanext-apply_permissions_for_service/ckanext/apply_permissions_for_service/views/__init__.py
+++ ckanext/ckanext-apply_permissions_for_service/ckanext/apply_permissions_for_service/views/__init__.py
@@ -3,71 +3,92 @@ from logging import getLogger
 
 log = getLogger(__name__)
 
+
 def index():
-    context = {u'user': plugins.toolkit.g.user, u'auth_user_obj': plugins.toolkit.g.userobj}
-    data_dict = {}
-    applications = plugins.toolkit.get_action('service_permission_application_list')(context, data_dict)
-    extra_vars = {
-            'sent_applications': applications.get('sent', []),
-            'received_applications': applications.get('received', [])
+    try:
+        context = {u'user': plugins.toolkit.g.user, u'auth_user_obj': plugins.toolkit.g.userobj}
+        data_dict = {}
+        applications = plugins.toolkit.get_action('service_permission_application_list')(context, data_dict)
+        extra_vars = {
+                'sent_applications': applications.get('sent', []),
+                'received_applications': applications.get('received', [])
+                }
+        return plugins.toolkit.render('apply_permissions_for_service/index.html', extra_vars=extra_vars)
+    except plugins.toolkit.NotAuthorized:
+        plugins.toolkit.abort(403, plugins.toolkit._(u'Not authorized to see this page'))
+
+
+def new_post(context, subsystem_id):
+    form = plugins.toolkit.request.form
+    data_dict = {
+            'organization': form.get('organization'),
+            'business_code': form.get('businessCode'),
+            'contact_name': form.get('contactName'),
+            'contact_email': form.get('contactEmail'),
+            'subsystem_id': form.get('subsystemId'),
+            'subsystem_code': form.get('subsystemCode'),
+            'service_code_list': form.getlist('serviceCode'),
+            'ip_address_list': form.get('ipAddress'),
+            'request_date': form.get('requestDate'),
+            'usage_description': form.get('usageDescription'),
             }
-    return plugins.toolkit.render('apply_permissions_for_service/index.html', extra_vars=extra_vars)
+
+    try:
+        plugins.toolkit.get_action('service_permission_application_create')(context, data_dict)
+    except plugins.toolkit.ValidationError as e:
+        # Todo: render form again with errors
+        return plugins.toolkit.render('apply_permissions_for_service/new.html')
+
+    return plugins.toolkit.render('apply_permissions_for_service/sent.html')
+
+
+def new_get(context, subsystem_id):
+    service_id = plugins.toolkit.request.args.get('service_id')
+    package = plugins.toolkit.get_action('package_show')(context, {'id': subsystem_id})
+    organization = plugins.toolkit.get_action('organization_show')(context, {'id': package['owner_org']})
+    user_managed_organizations = plugins.toolkit.get_action('organization_list_for_user')(context, {
+        'permission': 'manage_group'
+        })
+    user_managed_datasets_query = 'owner_org:({})'.format(' OR '.join(o['id'] for o in user_managed_organizations))
+    user_managed_datasets = plugins.toolkit.get_action('package_search')(context, {
+        'q': user_managed_datasets_query,
+        'include_private': True,
+        'rows': 1000
+        }).get('results', []) if user_managed_organizations else []
+
+    log.info(plugins.toolkit.g)
+    extra_vars = {
+            'subsystem_id': subsystem_id,
+            'service_id': service_id,
+            'pkg': package,
+            'service': next((r for r in package.get('resources', []) if r['id'] == service_id), None),
+            'org': organization,
+            'user': plugins.toolkit.g.userobj,
+            'user_managed_organizations': user_managed_organizations,
+            'user_managed_datasets': user_managed_datasets
+            }
+    return plugins.toolkit.render('apply_permissions_for_service/new.html', extra_vars=extra_vars)
+
 
 def new(subsystem_id):
-    context = {u'user': plugins.toolkit.g.user, u'auth_user_obj': plugins.toolkit.g.userobj}
-    if plugins.toolkit.request.method == u'POST':
-        form = plugins.toolkit.request.form
-        data_dict = {
-                'organization': form.get('organization'),
-                'business_code': form.get('businessCode'),
-                'contact_name': form.get('contactName'),
-                'contact_email': form.get('contactEmail'),
-                'subsystem_id': form.get('subsystemId'),
-                'subsystem_code': form.get('subsystemCode'),
-                'service_code_list': form.getlist('serviceCode'),
-                'ip_address_list': form.get('ipAddress'),
-                'request_date': form.get('requestDate'),
-                'usage_description': form.get('usageDescription'),
-                }
+    try:
+        context = {u'user': plugins.toolkit.g.user, u'auth_user_obj': plugins.toolkit.g.userobj}
+        plugins.toolkit.check_access('service_permission_application_create', context, {})
 
-        try:
-            plugins.toolkit.get_action('service_permission_application_create')(context, data_dict)
-        except plugins.toolkit.ValidationError as e:
-            # Todo: render form again with errors
-            return plugins.toolkit.render('apply_permissions_for_service/new.html')
-
-        return plugins.toolkit.render('apply_permissions_for_service/sent.html')
-    else:
-        service_id = plugins.toolkit.request.args.get('service_id')
-        package = plugins.toolkit.get_action('package_show')(context, {'id': subsystem_id})
-        organization = plugins.toolkit.get_action('organization_show')(context, {'id': package['owner_org']})
-        user_managed_organizations = plugins.toolkit.get_action('organization_list_for_user')(context, {
-            'permission': 'manage_group'
-            })
-        user_managed_datasets_query = 'owner_org:({})'.format(' OR '.join(o['id'] for o in user_managed_organizations))
-        user_managed_datasets = plugins.toolkit.get_action('package_search')(context, {
-            'q': user_managed_datasets_query,
-            'include_private': True,
-            'rows': 1000
-            }).get('results', []) if user_managed_organizations else []
-
-        log.info(plugins.toolkit.g)
-        extra_vars = {
-                'subsystem_id': subsystem_id,
-                'service_id': service_id,
-                'pkg': package,
-                'service': next((r for r in package.get('resources', []) if r['id'] == service_id), None),
-                'org': organization,
-                'user': plugins.toolkit.g.userobj,
-                'user_managed_organizations': user_managed_organizations,
-                'user_managed_datasets': user_managed_datasets
-                }
-        return plugins.toolkit.render('apply_permissions_for_service/new.html', extra_vars=extra_vars)
+        if plugins.toolkit.request.method == u'POST':
+            return new_post(context, subsystem_id)
+        else:
+            return new_get(context, subsystem_id)
+    except plugins.toolkit.NotAuthorized:
+        plugins.toolkit.abort(403, plugins.toolkit._(u'Not authorized to see this page'))
 
 def view(application_id):
-    context = {u'user': plugins.toolkit.g.user, u'auth_user_obj': plugins.toolkit.g.userobj}
-    data_dict = {'id': application_id}
-    application = plugins.toolkit.get_action('service_permission_application_show')(context, data_dict)
-    extra_vars = {'application': application}
-    return plugins.toolkit.render('apply_permissions_for_service/view.html', extra_vars=extra_vars)
+    try:
+        context = {u'user': plugins.toolkit.g.user, u'auth_user_obj': plugins.toolkit.g.userobj}
+        data_dict = {'id': application_id}
+        application = plugins.toolkit.get_action('service_permission_application_show')(context, data_dict)
+        extra_vars = {'application': application}
+        return plugins.toolkit.render('apply_permissions_for_service/view.html', extra_vars=extra_vars)
+    except plugins.toolkit.NotAuthorized:
+        plugins.toolkit.abort(403, plugins.toolkit._(u'Not authorized to see this page'))
 
