6a408d16822f596bedeb524912471845ced74048
Jari Voutilainen <jari.voutilainen@iki.fi>
2020-12-04 11:20:14 +0200

    LIKA-244: Add translations to cookie popup

--- ckanext/ckanext-apicatalog_ui/ckanext/apicatalog_ui/fanstatic/cookieconsent/ckan-cookieconsent.js
+++ ckanext/ckanext-apicatalog_ui/ckanext/apicatalog_ui/fanstatic/cookieconsent/ckan-cookieconsent.js
@@ -1,29 +1,33 @@
-window.cookieconsent.initialise({
-    container: document.getElementById("cookie_consent"),
-    position: "top",
-    type: "opt-in",
-    static: false,
-    theme: "suomifi",
-    onInitialise: function (status){
-        let type = this.options.type;
-        let didConsent = this.hasConsented();
-        if (type === 'opt-in' && didConsent) {
-            console.log("enable cookies")
-        }
-        if (type === 'opt-out' && !didConsent) {
-            console.log("disable cookies")
-        }
-    },
-    onStatusChange: function(status, chosenBefore) {
-        let type = this.options.type;
-        let didConsent = this.hasConsented();
-        if (type === 'opt-in' && didConsent) {
-            console.log("enable cookies")
-            window.location.reload();
-        }
-        if (type === 'opt-in' && !didConsent) {
-            console.log("disable cookies")
-            window.location.reload();
+ckan.module('cookie_consent', function (jQuery){
+    return {
+        initialize: function() {
+            window.cookieconsent.initialise({
+                container: document.getElementById("cookie_consent"),
+                position: "top",
+                type: "opt-in",
+                static: false,
+                theme: "suomifi",
+                content: {
+                    policy: this._('Cookie Policy'),
+                    message: this._('This website uses cookies to ensure you get the best experience on our website.'),
+                    allow: this._('Allow cookies'),
+                    deny: this._('Decline'),
+                    link: this._('Learn more')
+
+                },
+                onStatusChange: function(status, chosenBefore) {
+                    let type = this.options.type;
+                    let didConsent = this.hasConsented();
+                    if (type === 'opt-in' && didConsent) {
+                        console.log("enable cookies")
+                        window.location.reload();
+                    }
+                    if (type === 'opt-in' && !didConsent) {
+                        console.log("disable cookies")
+                        window.location.reload();
+                    }
+                }
+            })
         }
     }
 })
