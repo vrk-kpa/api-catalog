293a5680a15cf8284027db1ec6f0809232b5634b
Teemu Erkkola <teemu.erkkola@gofore.com>
2020-12-18 13:37:09 +0200

    LIKA-227: Remove dead code

--- ckanext/ckanext-apicatalog_ui/ckanext/apicatalog_ui/fanstatic/datetimepicker/datepicker.js
+++ ckanext/ckanext-apicatalog_ui/ckanext/apicatalog_ui/fanstatic/datetimepicker/datepicker.js
@@ -6,7 +6,6 @@
 ckan.module("datepicker", function($) {
   return {
     initialize: function() {
-      //this.el.find("[data-datepicker]").each(function() {
         let dp = $(this.el)
           .datetimepicker({
             format: "YYYY-MM-DD"
@@ -17,18 +16,6 @@ ckan.module("datepicker", function($) {
             this.el.parents('form:first').submit()
           });
         }
-          /*.on("dp.hide", function(e) {
-            if (!e.oldDate && e.date.isSame(moment(), "day")) {
-              $(".advanced-search-form").submit();
-            }
-
-          })
-          .on("dp.change", function(e) {
-            if (!e.date && e.oldDate || e.oldDate && !e.date.isSame(e.oldDate, "day")) {
-              $(".advanced-search-form").submit();
-            }
-        })*/;
-      //});
     }
   };
 });
