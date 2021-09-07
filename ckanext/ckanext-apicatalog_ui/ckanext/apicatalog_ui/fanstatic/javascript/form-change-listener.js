787d104d7f930fc4943084b438094599e1619df5
Pasi Laakso <pasi.laakso@gofore.com>
2021-09-07 10:29:05 +0300

    LIKA-375: Still ui changes. Removed reload-confirmation when submitting form. Fixed some styles and translations for manage permissions

--- ckanext/ckanext-apicatalog_ui/ckanext/apicatalog_ui/fanstatic/javascript/form-change-listener.js
+++ ckanext/ckanext-apicatalog_ui/ckanext/apicatalog_ui/fanstatic/javascript/form-change-listener.js
@@ -1,34 +1,39 @@
 this.ckan.module('form-change-listener', function($) {
   return {
     /* options object can be extended using data-module-* attributes */
-    options: {
-      confirmModalSelector: null, // selector for modal
-    },
+    options: {},
 
     initialize: function () {
-      console.log("Init", this);
-      if (this.options?.confirmModalSelector === null) {
-        throw new Error("Can't initialize form listener without modal selector");
-      }
-
-      this.confirmModal = document.querySelector(this.options.confirmModalSelector);
       this.listenChanges();
-      this.modal = new Modal(this.confirmModal);
     },
 
-    // Listen changes in form and set's state when changed
+    // Listen changes in form and initialize confirm dialog to prevent accidental page changes
     listenChanges: function() {
+      // If form is submitted, we shouldn't pop up dialog to confirm exit from page
+      // so unbind beforeunload in that case
+      this.el.submit(this.unbindBeforeunload);
       this.el.find(':input').change((function () {
+        // Binding beforeunload prevents browsers from caching Back-Forward cache, so init only once form changes
+        // https://developers.google.com/web/updates/2018/07/page-lifecycle-api#the-beforeunload-event
+        if (this.el.data('form-changed') !== true) {
+          this.bindBeforeunload();
+        }
         this.el.data('form-changed', true);
       }).bind(this));
+    },
 
-      // $(window).bind('beforeunload', (function(e) {
-      //   console.log(e);
-      //   if (this.el.data('form-changed') === true) {
-      //     return "Are you sure you want to exit page with unsaved changes?"
-      //   }
-      //   return;
-      // }).bind(this));
+    bindBeforeunload: function() {
+      $(window).bind('beforeunload', this.beforeunload.bind(this));
+    },
+
+    unbindBeforeunload: function() {
+      $(window).unbind('beforeunload', this.beforeunload);
+    },
+
+    beforeunload: function(e) {
+      console.log(e);
+      e.returnValue = "Are you sure you want to exit page with unsaved changes?";
+      return e;
     }
   }
 })
