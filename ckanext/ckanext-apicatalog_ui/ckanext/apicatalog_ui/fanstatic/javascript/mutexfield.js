093f37a1be572d5a57d3f427657edabc923b9c9b
Pasi Laakso <pasi.laakso@gofore.com>
2021-09-27 14:28:52 +0300

    LIKA-326: Fix issue with non functional upload button

--- ckanext/ckanext-apicatalog_ui/ckanext/apicatalog_ui/fanstatic/javascript/mutexfield.js
+++ ckanext/ckanext-apicatalog_ui/ckanext/apicatalog_ui/fanstatic/javascript/mutexfield.js
@@ -6,22 +6,26 @@
 ckan.module("mutexfield", function($) {
   return {
     initialize: function() {
-      $('[data-mutex-field]', this.el).each((i, el) => {
-        let children = $('[data-mutex-value]', el);
-        let mutexField = $(el).attr('data-mutex-field');
-        let mutexElement = $(`[name=${mutexField}]`);
+      // Added timeout to make mutexfield hide non-active sections after other modules are initialized
+      // so that this won't break dom-calculations.
+      setTimeout(() => {
+        $('[data-mutex-field]', this.el).each((i, el) => {
+          let children = $('[data-mutex-value]', el);
+          let mutexField = $(el).attr('data-mutex-field');
+          let mutexElement = $(`[name=${mutexField}]`);
 
-        const handleMutex = (newValue) => {
-          children.each((i, child) => {
-            let mutexValue = $(child).attr('data-mutex-value');
-            $(child).toggleClass('hidden', newValue != mutexValue);
+          const handleMutex = (newValue) => {
+            children.each((i, child) => {
+              let mutexValue = $(child).attr('data-mutex-value');
+              $(child).toggleClass('hidden', newValue != mutexValue);
+            });
+          };
+          handleMutex(mutexElement.val());
+          mutexElement.change((ev) => {
+            handleMutex(ev.target.value)
           });
-        };
-        handleMutex(mutexElement.val());
-        mutexElement.change((ev) => {
-          handleMutex(ev.target.value)
         });
-      });
+      }, 0);
     }
   };
 });
