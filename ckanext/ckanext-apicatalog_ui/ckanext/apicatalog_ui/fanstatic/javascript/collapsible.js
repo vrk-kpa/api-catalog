6e48e0df6c722e2b660844ee29419cb75d406d74
Atte Räty <atte.raty@gofore.com>
2021-12-08 10:22:57 +0200

    LIKA-410: Changed the arrow/chevron on dropdown info box to use font awesome chevrons instead of unicode arrow heads

--- ckanext/ckanext-apicatalog_ui/ckanext/apicatalog_ui/fanstatic/javascript/collapsible.js
+++ ckanext/ckanext-apicatalog_ui/ckanext/apicatalog_ui/fanstatic/javascript/collapsible.js
@@ -14,10 +14,19 @@ this.ckan.module('collapsible', function($) {
         elements[i].addEventListener('click', function () {
           this.classList.toggle('collapsible-active');
           var content = this.nextElementSibling;
+          var icons = $('.collapsible-icon');
           if (content.style.display === 'block') {
             content.style.display = 'none';
+            for (var i = 0; i < icons.length; i++) {
+              icons[i].classList.remove("fa-chevron-up")
+              icons[i].classList.add("fa-chevron-down")
+            }
           } else {
             content.style.display = 'block';
+            for (var i = 0; i < icons.length; i++) {
+              icons[i].classList.remove("fa-chevron-down")
+              icons[i].classList.add("fa-chevron-up")
+            }
           }
         });
       }
