70b19b19923e458c4cb9a3f5ed788908de7c02f6
Atte Räty <atte.raty@gofore.com>
2021-12-14 16:31:39 +0200

    LIKA-410: The new JS solution now also supports retrieving a prefilled title from the Title (fi) field

--- ckanext/ckanext-apicatalog_ui/ckanext/apicatalog_ui/fanstatic/javascript/title_to_del_title_and_url_slug.js
+++ ckanext/ckanext-apicatalog_ui/ckanext/apicatalog_ui/fanstatic/javascript/title_to_del_title_and_url_slug.js
@@ -16,34 +16,51 @@ this.ckan.module('title_to_del_title_and_url_slug', function($) {
 
     listenToChanges: function() {
       var el = $('#field-title_translated-fi');
+
+      // Run the code once in the case of the title being already filled in on page load (e.g. when opening the subsystem
+      // (in an edit mode)
+      $(window).on('load', function() {
+        this.processTitle(el[0]);
+      });
+
+      this.processTitle(el[0]);
+
+      // Set the input element to listen to changes to its value
       el.on('input', (function () {
-        var string = el[0].value;
-        var words = string.split(' ');
-        var capitalizedWords = '';
-        var dashSeparatedWords = '';
-
-        for (var i = 0; i < words.length; i++)
-        {
-          var capitalizedWord = this.processWord(words[i], true);
-          var word = this.processWord(words[i], false);
-
-          capitalizedWords = capitalizedWords + capitalizedWord;
-
-          if (i === 0) {
-            dashSeparatedWords = dashSeparatedWords + word;
-          } else {
-            dashSeparatedWords = dashSeparatedWords + "-" + word;
-          }
-        }
-
-        $('#title_in_data_exchange_layer')[0].value = capitalizedWords;
-        $('#dataset-url-slug')[0].textContent = dashSeparatedWords
+        this.processTitle(el[0]);
       }).bind(this));
     },
 
-    capitalize: function(str) {
-      var capitalizedCharacter = str.charAt(0).toUpperCase();
-      return capitalizedCharacter + str.slice(1);
+    processTitle: function(element) {
+      var capitalizedWords = this.processWords(element.value, true);
+      var dashSeparatedWords = this.processWords(element.value, false);
+
+      $('#title_in_data_exchange_layer')[0].value = capitalizedWords;
+      $('#dataset-url-slug')[0].textContent = dashSeparatedWords
+    },
+
+    // Yes, extracting the loop into a function results in it being run multiple times when it could only run once
+    // (well, twice) but it really shouldn't matter in this context
+    processWords: function(string, shouldBeCapitalized) {
+      var stringToProcess = string;
+      if (string === undefined) stringToProcess = '';
+      var words = stringToProcess.split(' ');
+      var wordsAfterProcessing = '';
+      for (var i = 0; i < words.length; i++)
+      {
+        var word = this.processWord(words[i], shouldBeCapitalized);
+        if (shouldBeCapitalized) {
+          wordsAfterProcessing = wordsAfterProcessing + word;
+        } else {
+          if (i === 0) {
+            wordsAfterProcessing = wordsAfterProcessing + word;
+          } else {
+            wordsAfterProcessing = wordsAfterProcessing + "-" + word;
+          }
+        }
+      }
+
+      return wordsAfterProcessing;
     },
 
     processWord: function(word, shouldBeCapitalized) {
@@ -59,6 +76,11 @@ this.ckan.module('title_to_del_title_and_url_slug', function($) {
       wordBeingProcessed = wordBeingProcessed.normalize('NFKD').replace(/[^\w]/g, '');
 
       return wordBeingProcessed;
+    },
+
+    capitalize: function(str) {
+      var capitalizedCharacter = str.charAt(0).toUpperCase();
+      return capitalizedCharacter + str.slice(1);
     }
   }
 })
