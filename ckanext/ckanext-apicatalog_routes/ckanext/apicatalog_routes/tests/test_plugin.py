86091dedfe1fc9ececb5c9e36a79866660525e90
Jari Voutilainen <jari.voutilainen@iki.fi>
2022-01-05 12:13:51 +0200

    Add test for deleting subsystem as normal user

--- ckanext/ckanext-apicatalog_routes/ckanext/apicatalog_routes/tests/test_plugin.py
+++ ckanext/ckanext-apicatalog_routes/ckanext/apicatalog_routes/tests/test_plugin.py
@@ -1,5 +1,24 @@
 """Tests for plugin.py."""
+import pytest
 
+from ckan.tests import factories
+import ckan.tests.helpers as helpers
+from ckan.plugins.toolkit import NotAuthorized
 
-def test_plugin():
-    pass
+@pytest.mark.ckan_config('ckan.plugins', 'apicatalog_routes')
+@pytest.mark.usefixtures('clean_db', 'with_plugins', 'with_request_context')
+class Apicatalog_Routes_Tests():
+    def non_sysadmins_should_not_be_able_to_delete_subsystems(self):
+        user = factories.User()
+        org_users = [{"name": user["name"], "capacity": "admin"}]
+        org = factories.Organization(users=org_users)
+
+        subsystem = factories.Dataset(
+            owner_org=org["id"]
+        )
+
+        context = {'ignore_auth': False, 'user': user['name']}
+
+        with pytest.raises(NotAuthorized):
+            helpers.call_action('package_delete', context, name=subsystem['name'])
+
