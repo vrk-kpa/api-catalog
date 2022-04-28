f567321f04123a2633facb0efa8bb52b3de02940
Jari Voutilainen <jari.voutilainen@iki.fi>
2022-04-28 15:56:28 +0300

    Update assertion

--- ckanext/ckanext-apicatalog_scheming/ckanext/apicatalog_scheming/tests/test_plugin.py
+++ ckanext/ckanext-apicatalog_scheming/ckanext/apicatalog_scheming/tests/test_plugin.py
@@ -12,11 +12,11 @@ def test_allowed_organization_user_should_see_subsystem():
 
     organization2 = Organization(users=org2_users)
 
-    Dataset(private=True, owner_org=organization1['id'], allowed_organizations=organization2['name'])
+    dataset = Dataset(private=True, owner_org=organization1['id'], allowed_organizations=organization2['name'])
 
     results = get_action(u"package_search")(
             {u"user": user2["name"]}, {u"include_private": True}
     )["results"]
 
     names = [r["name"] for r in results]
-    assert names == [u"d1"]
+    assert names == [dataset['name']]
