e7d8797f7fd27fae57c4811734f8f6dfc3bec034
Jari Voutilainen <jari.voutilainen@iki.fi>
2022-04-28 15:34:02 +0300

    Add first test of subsystems

--- ckanext/ckanext-apicatalog_scheming/ckanext/apicatalog_scheming/tests/test_plugin.py
+++ ckanext/ckanext-apicatalog_scheming/ckanext/apicatalog_scheming/tests/test_plugin.py
@@ -1,5 +1,22 @@
-"""Tests for plugin.py."""
+import pytest
+from ckan.tests.factories import User, Dataset, Organization
+from ckan.plugins.toolkit import get_action
 
 
-def test_plugin():
-    pass
+@pytest.mark.usefixtures('with_plugins', 'clean_db')
+def test_allowed_organization_user_should_see_subsystem():
+    organization1 = Organization()
+
+    user2 = User()
+    org2_users = [{"name": user2["name"], "capacity": "admin"}]
+
+    organization2 = Organization(users=org2_users)
+
+    Dataset(private=True, owner_org=organization1['id'], allowed_organizations=organization2['name'])
+
+    results = get_action(u"package_search")(
+            {u"user": user2["name"]}, {u"include_private": True}
+    )["results"]
+
+    names = [r["name"] for r in results]
+    assert names == [u"d1"]
