bd63663361a03ce28bb0c9dfb90a06c45082ec41
Pasi Laakso <pasi.laakso@gofore.com>
2022-05-02 15:06:20 +0300

    LIKA-415: Add 6535 patch about email attachments. Include additional file in email that is sent when permission application is created

--- ckanext/ckanext-apply_permissions_for_service/ckanext/apply_permissions_for_service/helpers.py
+++ ckanext/ckanext-apply_permissions_for_service/ckanext/apply_permissions_for_service/helpers.py
@@ -1,4 +1,5 @@
 from ckan.plugins import toolkit
+import ckan.lib.uploader as uploader
 
 
 def service_permission_application_url(subsystem_id):
@@ -11,3 +12,6 @@ def service_permission_applications_enabled(subsystem_id):
     settings = toolkit.get_action('service_permission_settings_show')({'ignore_auth': True},
                                                                       {'subsystem_id': subsystem_id})
     return True if settings and settings.get('delivery_method', 'none') != 'none' else False
+
+def get_application_attachment(filename):
+    return open('{}/storage/uploads/apply_permission/{}'.format(uploader.get_storage_path(), filename), 'rb') 
