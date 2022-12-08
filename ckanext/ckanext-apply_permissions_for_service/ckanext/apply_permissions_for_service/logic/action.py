65c8d4de87baf5ce899a0f97edd3e61cfc350162
Eetu Mansikkamäki <eetu.mansikkamaki@gofore.com>
2022-12-08 14:16:27 +0200

    LIKA-553: Add optional guide text to apply permission settings

--- ckanext/ckanext-apply_permissions_for_service/ckanext/apply_permissions_for_service/logic/action.py
+++ ckanext/ckanext-apply_permissions_for_service/ckanext/apply_permissions_for_service/logic/action.py
@@ -221,7 +221,8 @@ def service_permission_settings_update(context, data_dict):
 
     settings = {field: data_dict[field]
                 for field in ('delivery_method', 'api', 'web', 'email', 'file_url', 'original_filename',
-                              'require_additional_application_file', 'additional_file_url', 'original_additional_filename')
+                              'require_additional_application_file', 'additional_file_url', 'original_additional_filename',
+                              'guide_text_translated')
                 if field in data_dict}
 
     tk.get_action('package_patch')(context, {
