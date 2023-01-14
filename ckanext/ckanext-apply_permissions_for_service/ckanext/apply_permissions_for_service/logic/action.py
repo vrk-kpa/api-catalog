26dda52536fbd683962a6fc3d92989911d56f07e
Eetu Mansikkamäki <eetu.mansikkamaki@gofore.com>
2023-01-14 21:52:35 +0200

    LIKA-572: Refactoring for clarity

--- ckanext/ckanext-apply_permissions_for_service/ckanext/apply_permissions_for_service/logic/action.py
+++ ckanext/ckanext-apply_permissions_for_service/ckanext/apply_permissions_for_service/logic/action.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import requests
 import json
 
 from ckan.plugins import toolkit as tk
@@ -27,28 +26,28 @@ def service_permission_application_create(context, data_dict):
     if organization_id is None or organization_id == "":
         errors['organization_id'] = _('Missing value')
 
-    intermediate_organization_id = data_dict.get('intermediate_organization')
+    intermediate_organization_id = data_dict.get('intermediate_organization_id')
 
     target_organization_id = data_dict.get('target_organization_id')
     if target_organization_id is None or target_organization_id == "":
         errors['target_organization_id'] = _('Missing value')
 
-    business_code = data_dict.get('business_code')
-    if business_code is None or business_code == "":
-        errors['business_code'] = _('Missing value')
+    member_code = data_dict.get('member_code')
+    if member_code is None or member_code == "":
+        errors['member_code'] = _('Missing value')
     else:
         try:
             # From ckanext-apicatalog
-            tk.get_validator('business_id_validator')(business_code)
+            tk.get_validator('business_id_validator')(member_code)
         except tk.Invalid as e:
-            errors['business_code'] = e.error
+            errors['member_code'] = e.error
 
-    intermediate_business_code = data_dict.get('intermediate_business_code')
-    if intermediate_business_code:
+    intermediate_member_code = data_dict.get('intermediate_member_code')
+    if intermediate_member_code:
         try:
-            tk.get_validator('business_id_validator')(intermediate_business_code)
+            tk.get_validator('business_id_validator')(intermediate_member_code)
         except tk.Invalid as e:
-            errors['intermediate_business_code'] = e.error
+            errors['intermediate_member_code'] = e.error
 
     contact_name = data_dict.get('contact_name')
     if contact_name is None or contact_name == "":
@@ -66,23 +65,23 @@ def service_permission_application_create(context, data_dict):
     ip_address_list = data_dict.get('ip_address_list')
     if not (isinstance(ip_address_list, list) and any(ip_address_list)):
         errors['ip_address_list'] = _('Missing value')
+    target_subsystem_id = data_dict.get('target_subsystem_id')
+    if target_subsystem_id is None or target_subsystem_id == "":
+        errors['target_subsystem_id'] = _('Missing value')
+
+    # subsystem_id (previously subsystem_code) must exist and match subsystem belonging to the selected utilizing organization
     subsystem_id = data_dict.get('subsystem_id')
     if subsystem_id is None or subsystem_id == "":
         errors['subsystem_id'] = _('Missing value')
-
-    # Subsystem code must exist and match a subsystem belonging to the selected utilizing organization
-    subsystem_code = data_dict.get('subsystem_code')
-    if subsystem_code is None or subsystem_code == "":
-        errors['subsystem_code'] = _('Missing value')
     else:
-        utilizing_organization_business_code = intermediate_business_code or business_code
-        subsystem = tk.get_action('package_show')(context, {'id': subsystem_code})
-        if not subsystem.get('owner_org', '').endswith(utilizing_organization_business_code):
-            errors['subsystem_code'] = _('Selected subsystem does not belong to the utilizing organization')
+        utilizing_organization_member_code = intermediate_member_code or member_code
+        subsystem = tk.get_action('package_show')(context, {'id': subsystem_id})
+        if not subsystem.get('owner_org', '').endswith(utilizing_organization_member_code):
+            errors['subsystem_id'] = _('Selected subsystem does not belong to the utilizing organization')
 
-    service_code_list = data_dict.get('service_code_list')
-    if not service_code_list:
-        errors['service_code_list'] = _('Missing value')
+    service_id_list = data_dict.get('service_id_list')
+    if not service_id_list:
+        errors['service_id_list'] = _('Missing value')
 
     if errors:
         raise tk.ValidationError(errors)
@@ -99,14 +98,14 @@ def service_permission_application_create(context, data_dict):
     application_id = model.ApplyPermission.create(organization_id=organization_id,
                                                   target_organization_id=target_organization_id,
                                                   intermediate_organization_id=intermediate_organization_id,
-                                                  business_code=business_code,
-                                                  intermediate_business_code=intermediate_business_code,
+                                                  member_code=member_code,
+                                                  intermediate_member_code=intermediate_member_code,
                                                   contact_name=contact_name,
                                                   contact_email=contact_email,
                                                   ip_address_list=ip_address_list,
+                                                  target_subsystem_id=target_subsystem_id,
                                                   subsystem_id=subsystem_id,
-                                                  subsystem_code=subsystem_code,
-                                                  service_code_list=service_code_list,
+                                                  service_id_list=service_id_list,
                                                   usage_description=usage_description,
                                                   request_date=request_date,
                                                   application_filename=application_filename)
@@ -114,22 +113,7 @@ def service_permission_application_create(context, data_dict):
     service_permission_settings = package.get('service_permission_settings', {})
     delivery_method = service_permission_settings.get('delivery_method', 'email')
 
-    if delivery_method == 'api':
-        application = model.ApplyPermission.get(application_id).as_dict()
-        try:
-            api_url = service_permission_settings.get('api')
-
-            data = data_dict.copy()
-            data['subsystem_code'] = package.get('xroad_subsystemcode') or package['title']
-            service_code_list = [r['xroad_servicecode'] or r['name'] for r in package.get('resources')
-                                 if r['id'] in data_dict['service_code_list']]
-            data['service_code_list'] = service_code_list
-
-            requests.post(api_url, data=json.dumps(data), timeout=5).raise_for_status()
-        except Exception as e:
-            log.error('Error calling request application API: %s', e)
-
-    elif delivery_method == 'email':
+    if delivery_method == 'email':
         email_address = service_permission_settings.get('email', owner_org.get('email_address'))
         if email_address:
             log.info('Sending permission application notification email to {}'.format(email_address))
@@ -159,13 +143,13 @@ def service_permission_application_list(context, data_dict):
     check_access('service_permission_application_list', context, data_dict)
     applications = ckan_model.Session.query(model.ApplyPermission)
 
-    subsystem_id = data_dict.get('subsystem_id')
-    if subsystem_id:
-        applications = applications.filter(model.ApplyPermission.subsystem_id == subsystem_id)
+    target_subsystem_id = data_dict.get('target_subsystem_id')
+    if target_subsystem_id:
+        applications = applications.filter(model.ApplyPermission.target_subsystem_id == target_subsystem_id)
 
     applications = applications.all()
 
-    if not subsystem_id:
+    if not target_subsystem_id:
         membership_organizations = tk.get_action('organization_list_for_user')(context, {'permission': 'read'})
         organization_id_list = [org['id'] for org in membership_organizations]
         response = {'received': [], 'sent': []}
