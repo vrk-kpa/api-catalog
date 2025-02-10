233db3d982976d1de0fa97b347c03cd03272a395
Teemu Erkkola <teemu.erkkola@gofore.com>
2025-02-10 15:34:24 +0200

    LIKA-636: Fix LinkValidationResult.get_for_organization_since join

--- ckanext/ckanext-validate_links/ckanext/validate_links/model/__init__.py
+++ ckanext/ckanext-validate_links/ckanext/validate_links/model/__init__.py
@@ -40,7 +40,7 @@ class LinkValidationResult(toolkit.BaseModel):
     @classmethod
     def get_for_organization_since(cls, organization_id, t):
         return (model.Session.query(cls)
-                .join(link_validation_referrer_table)
+                .join(LinkValidationReferrer)
                 .filter(LinkValidationReferrer.organization == organization_id)
                 .filter(cls.timestamp > t)
                 .all())
