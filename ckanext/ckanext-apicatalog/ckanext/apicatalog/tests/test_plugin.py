54539270139bb549c62b9e22aa34eea0924b6722
Teemu Erkkola <teemu.erkkola@gofore.com>
2025-04-23 09:28:38 +0300

    LIKA-687: Add test for entering a string as old business ids

--- ckanext/ckanext-apicatalog/ckanext/apicatalog/tests/test_plugin.py
+++ ckanext/ckanext-apicatalog/ckanext/apicatalog/tests/test_plugin.py
@@ -230,6 +230,11 @@ class TestApicatalogPlugin():
         results = helpers.call_action('create_organization_users', context)['result']
         assert results['added'] == [user['name']]
 
+    def test_old_business_ids_output_with_string(self):
+        organization_with_single_olf_business_id = Organization(old_business_ids='["1","2"]')
+
+        assert organization_with_single_olf_business_id['old_business_ids'] == ['["1","2"]']
+
     def test_old_business_ids_output_with_list(self):
         organization_with_python_list = Organization(old_business_ids=['1', '2'])
 
